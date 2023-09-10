9e72045c95964b892d3f0b07012bb961bcb8eda4
ilyes rafai <rafaiilyes3@gmail.com>
2023-09-10 16:04:29 +0100

    Create Poppup to display the winner name and for restarting the game

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import Matrix from './components/Matrix';
-import Players from './components/Players';
-import Footer from './components/Footer';
-import Score from './components/Score';
-import Title from './components/Title';
-import { useState } from 'react';
-import Confetti from 'react-confetti';
-import useWindowSize from 'react-use/lib/useWindowSize';
-
+import { useState } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
+import "./App.css";
+import Footer from "./components/Footer";
+import Matrix from "./components/Matrix";
+import Players from "./components/Players";
+import PopupWinner from "./components/PopupWinner";
+import Score from "./components/Score";
+import Title from "./components/Title";
+import logo from "./logo.svg";
 
 function App() {
-  
-  
+    const [game, setGame] = useState({
+        player1: {
+            name: "Imad",
+            score: 0,
+            letter: "X",
+        },
+        player2: {
+            name: "Ilyes",
+            score: 0,
+            letter: "O",
+        },
+        current_turn: 0,
+        still_playing: true,
+    });
+    const { width, height } = useWindowSize();
 
-  const [game, setGame] = useState({
-    player1: {
-      name: 'Imad',
-      score: 0,
-      letter: 'X',
-    },
-    player2: {
-      name: 'Ilyes',
-      score: 0,
-      letter: 'O',
-    },
-    current_turn: 0,
-    still_playing: true,
-  });
-  const { width, height } = useWindowSize();
-
-
-  return (
-    <div className="App">
-      { !game.still_playing ? <Confetti width={width} height={height} /> : <></>}
-      <Title />
-      <Score score_p1={game.player1.score} score_p2={game.player2.score} />
-      <Matrix setGame={setGame} game={game} />
-      <Players name_p1={game.name_p1} name_p2={game.name_p2} />
-      <Footer game={game} />
-    </div>
-  );
+    return (
+        <div className="App">
+            {!game.still_playing ? (
+                <Confetti width={width} height={height} />
+            ) : (
+                <></>
+            )}
+            <PopupWinner />
+            <Title />
+            <Score
+                score_p1={game.player1.score}
+                score_p2={game.player2.score}
+            />
+            <Matrix setGame={setGame} game={game} />
+            <Players name_p1={game.name_p1} name_p2={game.name_p2} />
+            <Footer game={game} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/PopupWinner.jsx
+++ src/components/PopupWinner.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function PopupWinner() {
-  return (
-    <div className='_popup_wrapper'>
-        <div className='_popup_overlay'></div>
-        <div className='_popup_content'>
-            <div className='_popup_content_header'> 
-                <div className='_popup_content_header_winner'>
-                    <h4> The winner is : </h4> <h2> Ilyes </h2>
+    return (
+        <div className="_popup_wrapper">
+            <div className="_popup_overlay"></div>
+            <div className="_popup_content">
+                <div className="_popup_content_header">
+                    <div className="_popup_content_header_winner">
+                        <h4> The winner is : </h4> <h2> Ilyes </h2>
+                    </div>
                 </div>
-                <div className='_button'>X</div>
-            </div>
-            <div className='_popup_content_body'>
-                <button className='_popup_content_body_restart _button'> Restart </button>
+                <div className="_popup_content_body">
+                    <button className="_popup_content_body_restart _button">
+                        {" "}
+                        Restart the game{" "}
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default PopupWinner
+export default PopupWinner;
